c18e39ebc53d1676a51143255fef89d5feb93d40
sxl1 <rew2540@gmail.com>
2026-07-18 06:11:06 +0900

    add if break loop

--- random-number/random_number.py
+++ random-number/random_number.py
@@ -7,6 +7,11 @@ secret  =  random.randint(1,10)
 while True:
     guess = int(input("ท้ายเลข1-10: "))
 
+    #break loop 
+    if guess == 0 :
+        print("หยุดท้ายเลข")
+        break
+
     if guess == secret:
         print("ยินดีด้วยคุณท้ายถูก: ")
         break # ถ้ามันถูกจะออกจาก loop ทันที
