c9e950c4c6cd83d4aa935423f77e6a102eefd9d1
sxl1 <rew2540@gmail.com>
2026-07-21 05:49:41 +0900

    fix if hp

--- random-number/random_number.py
+++ random-number/random_number.py
@@ -12,20 +12,19 @@ while True:
         print("หยุดท้ายเลข")
         break
 
-    if hp == 0:
-        print("game over!")
-        print("number is :",secret)
-        break
-
     if guess == secret:
         score += 10
         print("ยินดีด้วยคุณท้ายถูก: ")
         break # ถ้ามันถูกจะออกจาก loop ทันที
     elif guess < secret:
-        print("มากกว่านี้เกินไป")
+        print("มากกว่านี้")
     else:
         print("น้อยกว่านี้")
     hp -= 1
     print(f"เหลืิอพลังชีวิต: {hp}")
 
+    if hp <= 0:
+        print("game over!")
+        print("number is :",secret)
+        break
     
